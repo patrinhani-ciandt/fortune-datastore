6391b106ee243c316d807e55c81734be4c7a04ac
Vinicius Patrinhani <patrinhani@ciandt.com>
2017-08-03 19:45:23 -0300

    updt

--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@ const testAdapter = require('fortune/test/adapter');
 const adapter = require('../lib');
 
 testAdapter(adapter, {
-  projectId: 'my-project-id',
-  keyFilename: __dirname + '/../gae_key.json',
+  projectId: 'fortune-datastore-unit-tests',
+  apiEndpoint: 'http://localhost:8888',
   namespace: 'fortune-adapter-test',
   generateId: () => Math.floor(Math.random() * Math.pow(2, 32)).toString(16)
 });
